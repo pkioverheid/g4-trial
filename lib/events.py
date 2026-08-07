2996f8b8955008ea40bb4c6e923dc9dc52b62ebd
Ronald van Rij <ronaldvanrij@users.noreply.github.com>
2026-08-07 09:11:03 +0000

    Add additional logdata and provide lookup function

--- lib/events.py
+++ lib/events.py
@@ -1,3 +1,4 @@
+import csv
 import logging
 import os
 
@@ -11,6 +12,7 @@ if not os.path.isdir('ca'):
 
 LOG_FILENAME = os.path.join('ca', 'events.txt')
 
+
 def configure_logger(logger, log_filename=LOG_FILENAME):
     file_handler = logging.FileHandler(filename=log_filename)
     formatter = logging.Formatter('%(asctime)s;%(name)s;%(levelname)s;%(message)s')
@@ -22,21 +24,36 @@ eventlog = logging.getLogger('events')
 configure_logger(eventlog)
 
 
-def log(msg: str, eventlog=eventlog):
+def log(msg: str, eventlog=eventlog) -> None:
     eventlog.info(msg)
 
 
-def _log_issued_cert(issuer_dn: str, aki: str, serial_number: int, enrollment: str, eventlog=eventlog):
-    log(f"issued;{issuer_dn};{aki};{serial_number};{enrollment}", eventlog=eventlog)
+def _log_issued_cert(issuer_enrollment: str, issuer_dn: str, aki: str, cert_serial_number: int, enrollment: str, eventlog=eventlog) -> None:
+    log(f"issued;{issuer_enrollment};{issuer_dn};{aki};{cert_serial_number};{enrollment}", eventlog=eventlog)
 
 
-def log_issued_cert(issuer: KeyPair, subject: KeyPair, eventlog=eventlog):
+def log_issued_cert(issuer: KeyPair, subject: KeyPair, eventlog=eventlog) -> None:
     cert = subject.certificate
     aki = cert.extensions.get_extension_for_class(AuthorityKeyIdentifier).value.key_identifier.hex()
-    _log_issued_cert(issuer.certificate.subject.rfc4514_string(), aki, cert.serial_number, subject.basename, eventlog=eventlog)
+    _log_issued_cert(issuer.basename, issuer.certificate.subject.rfc4514_string(), aki, cert.serial_number, subject.basename, eventlog=eventlog)
 
 
-def log_signed_crl(crl: x509.CertificateRevocationList, eventlog=eventlog):
+def log_signed_crl(crl: x509.CertificateRevocationList, eventlog=eventlog) -> None:
     aki = crl.extensions.get_extension_for_class(AuthorityKeyIdentifier).value.key_identifier.hex()
     crl_number = crl.extensions.get_extension_for_class(x509.CRLNumber).value.crl_number
     log(f"crl;{aki};{crl_number}", eventlog=eventlog)
+
+
+def lookup(issuer_dn: str, serial_number: int, logfile=LOG_FILENAME) -> str:
+    """
+    Looks up the basename of a certificate by its issuer DN and serial number in the event log file.
+    """
+    with open(logfile, encoding="utf-8") as f:
+        for row in csv.reader(f, delimiter=";"):
+            if row[3] != "issued":
+                continue
+
+            if row[5] == issuer_dn and row[7] == str(serial_number):
+                return row[4],row[8]
+
+    raise ValueError("Certificate not found")
