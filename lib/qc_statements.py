bee5f16b48db3ca329a0199c14ca1e0f387ceaa3
Ronald van Rij <ronaldvanrij@users.noreply.github.com>
2026-08-11 14:45:13 +0000

    Add unittests for encoding qcstatements

--- lib/qc_statements.py
+++ lib/qc_statements.py
@@ -7,6 +7,8 @@ from asn1crypto.core import (
     SequenceOf,
 )
 
+from lib.config import Config
+
 
 class SemanticsInformation(Sequence):
     _fields = [('semanticsIdentifier', ObjectIdentifier)] # noqa: RUF012
@@ -38,7 +40,7 @@ class PdsLocations(SequenceOf):
     _child_spec = PdsLocation
 
 
-def build_qc_statements_extension(qc_data, config):
+def build_qc_statements_extension(qc_data, config: Config) -> bytes:
     """
     Encodes QCStatements with a statementId and statementInfo (OID).
     """
@@ -55,7 +57,7 @@ def build_qc_statements_extension(qc_data, config):
                     'statementInfo': QcTypeSyntax([ObjectIdentifier(item['value'].split(' ')[0])])
                 }))
             case 'id-etsi-qcs-QcPDS':
-                loc = config.get('pdsLocation')
+                loc = config.pds_location
 
                 url = item['value']['url'].format(loc.get('url'))
                 language = item['value']['language'].format(loc.get('language'))
